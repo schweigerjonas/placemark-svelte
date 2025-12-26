06f3568b195086e8969128cd451f615def1bd32e
Jonas Schweiger <jonas.schweiger@outlook.de>
2025-12-26 11:44:26 +0100

    feat: add login form

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,34 +1,17 @@
+<script lang="ts">
+	import LoginForm from "./LoginForm.svelte";
+</script>
+
 <div class="flex h-full items-center justify-center">
 	<div class="card p-4 shadow">
-		<div class="card-body">
-			<h2 class="card-title mb-4 text-center">Welcome back!</h2>
-			<form method="POST">
-				<div class="form-floating mb-3">
-					<input
-						type="text"
-						class="form-control"
-						id="username"
-						name="username"
-						placeholder="Username"
-						required
-					/>
-					<label for="username" class="form-label">Username</label>
-				</div>
-				<div class="form-floating mb-3">
-					<input
-						type="password"
-						class="form-control"
-						id="password"
-						name="password"
-						placeholder="Password"
-						required
-					/>
-					<label for="password" class="form-label">Password</label>
-				</div>
-				<button type="submit" class="btn btn-primary w-100">
-					<strong>Log In</strong>
-				</button>
-			</form>
+		<div class="card-body flex flex-col gap-3">
+			<div>
+				<h1 class="card-title text-center">Welcome back!</h1>
+				<p class="text-muted text-center text-xs">
+					Enter your email and password to access your account.
+				</p>
+			</div>
+			<LoginForm />
 			<div class="mt-3 text-center">
 				<small class="text-muted">
 					Don't have an account?
---
+++ src/routes/login/LoginForm.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	import { goto } from "$app/navigation";
+	import ErrorNotification from "$lib/components/ErrorNotification.svelte";
+	import UserCredentials from "$lib/components/UserCredentials.svelte";
+
+	let email = $state("");
+	let password = $state("");
+	let notification = $state("");
+
+	async function login() {
+		const success = false;
+
+		if (success) {
+			goto("/");
+		} else {
+			notification = "Invalid email or password.";
+		}
+	}
+</script>
+
+{#if notification}
+	<ErrorNotification {notification} />
+{/if}
+<div>
+	<UserCredentials bind:email bind:password />
+	<button onclick={() => login()} class="btn btn-primary w-100 p-2">
+		<strong>Log In</strong>
+	</button>
+</div>
