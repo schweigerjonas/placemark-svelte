bca43f75bdce2dcca7950e7b109b73b6cfd2c7d5
Jonas Schweiger <jonas.schweiger@outlook.de>
2026-01-07 11:26:52 +0100

    refactor: update navbar button styles and adjust content width and placement
    On ultrawide monitors, the logo and buttons are now no longer completely
    on the sides, but rather indented the same amount as the content

--- src/routes/(app)/Navbar.svelte
+++ src/routes/(app)/Navbar.svelte
@@ -2,50 +2,72 @@
 	import { loggedInUser } from "$lib/runes.svelte";
 </script>
 
-<nav class="navbar navbar-expand-sm bg-body-tertiary">
-	<div class="container-fluid">
-		<a class="navbar-brand flex items-center gap-2" href="/">
-			<span class="material-symbols-outlined"> globe_location_pin </span>
-			<span class="font-bold">Placemark</span>
-		</a>
+<div class="bg-body-tertiary flex flex-1 justify-center">
+	<div class="w-7xl">
+		<nav class="navbar navbar-expand-sm bg-body-tertiary">
+			<div class="container-fluid">
+				<a class="navbar-brand flex items-center gap-2" href="/">
+					<span class="material-symbols-outlined"> globe_location_pin </span>
+					<span class="font-bold">Placemark</span>
+				</a>
 
-		<div class="flex items-center justify-end">
-			{#if loggedInUser.role === "ADMIN"}
-				<a href="/admin-dashboard" class="btn btn-light">Admin Dashboard</a>
-			{/if}
-			{#if loggedInUser.email}
-				<div class="dropdown">
-					<button
-						class="btn btn-light flex items-center justify-center p-2"
-						data-bs-toggle="dropdown"
-						aria-expanded="false"
-					>
-						<div class="dropdown-toggle flex items-center justify-center gap-1.5">
-							<span>{loggedInUser.email}</span>
+				<div class="flex items-center justify-end gap-2">
+					{#if loggedInUser.role === "ADMIN"}
+						<a href="/admin-dashboard" class="btn btn-dark">
+							<div class="flex items-center gap-2">
+								<span class="material-symbols-outlined">admin_panel_settings</span>
+								<span>Admin Dashboard</span>
+							</div>
+						</a>
+					{/if}
+					{#if loggedInUser.email}
+						<a href="/collection" class="btn btn-primary">
+							<div class="flex items-center gap-2">
+								<span class="material-symbols-outlined">bookmarks</span>
+								<span>My Collection</span>
+							</div>
+						</a>
+					{/if}
+					{#if loggedInUser.email}
+						<div class="dropdown">
+							<button
+								class="btn btn-light flex items-center justify-center p-2"
+								data-bs-toggle="dropdown"
+								aria-expanded="false"
+							>
+								<div class="dropdown-toggle flex items-center justify-center gap-1.5">
+									<span>{loggedInUser.email}</span>
+								</div>
+							</button>
+							<ul class="dropdown-menu dropdown-menu-end">
+								<li>
+									<a class="dropdown-item" href="/profile">
+										<div class="flex items-center gap-2">
+											<span class="material-symbols-outlined">account_circle</span>
+											<span>Profile</span>
+										</div>
+									</a>
+								</li>
+								<li>
+									<a class="dropdown-item" href="/logout">
+										<div class="flex items-center gap-2">
+											<span class="material-symbols-outlined">logout</span>
+											<span>Logout</span>
+										</div>
+									</a>
+								</li>
+							</ul>
 						</div>
-					</button>
-					<ul class="dropdown-menu dropdown-menu-end">
-						<li>
-							<a class="dropdown-item" href="/profile">
-								<div class="flex items-center gap-2">
-									<span class="material-symbols-outlined">account_circle</span>
-									<span>Profile</span>
-								</div>
-							</a>
-						</li>
-						<li>
-							<a class="dropdown-item" href="/logout">
-								<div class="flex items-center gap-2">
-									<span class="material-symbols-outlined">logout</span>
-									<span>Logout</span>
-								</div>
-							</a>
-						</li>
-					</ul>
+					{:else}
+						<a href="/login" class="btn btn-outline-primary">
+							<div class="flex items-center gap-2">
+								<span class="material-symbols-outlined">login</span>
+								<span>Login</span>
+							</div>
+						</a>
+					{/if}
 				</div>
-			{:else}
-				<a href="/login" class="btn btn-secondary"> Login </a>
-			{/if}
-		</div>
+			</div>
+		</nav>
 	</div>
-</nav>
+</div>
