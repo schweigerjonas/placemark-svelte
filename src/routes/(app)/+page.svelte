975bb060f56df85aa31ea3352bb6c7adf7b2c403
Jonas Schweiger <jonas.schweiger@outlook.de>
2026-01-07 07:58:28 +0100

    feat: add example chart

--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -4,6 +4,16 @@
 	import { refreshMap } from "$lib/services/utils";
 	import { onMount } from "svelte";
 	import POIDetailCard from "./POIDetailCard.svelte";
+	import Chart from "svelte-frappe-charts";
+
+	const chartData = {
+		labels: ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"],
+		datasets: [
+			{
+				values: [10, 12, 3, 9, 8, 15, 9]
+			}
+		]
+	};
 
 	let map: LeafletMap;
 
@@ -12,13 +22,21 @@
 	});
 </script>
 
-<div class="flex items-center gap-2">
-	<div class="min-w-2/3 grow pl-2">
-		<LeafletMap height={80} zoom={7} bind:this={map} />
+<div class="flex flex-col gap-4">
+	<div class="flex items-center gap-2">
+		<div class="min-w-2/3 grow pl-2">
+			<LeafletMap height={80} zoom={7} bind:this={map} />
+		</div>
+		<div class="pr-2">
+			{#if selectedMarker.focused}
+				<POIDetailCard />
+			{/if}
+		</div>
 	</div>
-	<div class="pr-2">
-		{#if selectedMarker.focused}
-			<POIDetailCard />
-		{/if}
+	<div>
+		<h3>Statistics</h3>
+		<div class="w-1/2">
+			<Chart data={chartData} type="line" />
+		</div>
 	</div>
 </div>
