6d3485764c440febfe5eca5ab31a6888e82ff352
Jonas Schweiger <jonas.schweiger@outlook.de>
2026-01-17 23:31:31 +0100

    refactor: use server side redirect after logout

--- src/routes/(auth)/logout/+page.svelte
+++ src/routes/(auth)/logout/+page.svelte
@@ -1,12 +1,8 @@
 <script lang="ts">
-	import { browser } from "$app/environment";
-	import { goto } from "$app/navigation";
 	import { clearState } from "$lib/services/utils";
 	import { onMount } from "svelte";
 
 	onMount(() => {
 		clearState();
 	});
-
-	if (browser) goto("/");
 </script>
