77cca95ac01205c471d16fd76845b52fd1874c25
Jonas Schweiger <jonas.schweiger@outlook.de>
2026-01-17 23:58:51 +0100

    refactor: remove last change for logout

--- src/routes/(auth)/logout/+page.server.ts
+++ src/routes/(auth)/logout/+page.server.ts
@@ -1,8 +1,5 @@
-import { redirect } from "@sveltejs/kit";
 import type { PageServerLoad } from "./$types";
 
 export const load: PageServerLoad = async ({ cookies }) => {
 	cookies.delete("placemark-user", { path: "/" });
-
-	throw redirect(302, "/");
 };
