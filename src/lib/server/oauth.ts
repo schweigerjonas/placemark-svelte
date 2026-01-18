23dfaddf1eeaa40e3611408b2df6f7ed3d27d86b
Jonas Schweiger <jonas.schweiger@outlook.de>
2026-01-18 03:03:25 +0100

    feat: add dynamic callback url for oauth

--- src/lib/server/oauth.ts
+++ src/lib/server/oauth.ts
@@ -5,11 +5,18 @@ import {
 	GOOGLE_CLIENT_ID,
 	GOOGLE_CLIENT_SECRET
 } from "$env/static/private";
+import { dev } from "$app/environment";
 
-export const github = new GitHub(GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET, null);
+const baseURL = dev ? "http://localhost:5173" : "https://placemark-sve.netlify.app";
+
+export const github = new GitHub(
+	GITHUB_CLIENT_ID,
+	GITHUB_CLIENT_SECRET,
+	`${baseURL}/login/github/callback`
+);
 
 export const google = new Google(
 	GOOGLE_CLIENT_ID,
 	GOOGLE_CLIENT_SECRET,
-	"http://localhost:5173/login/google/callback"
+	`${baseURL}/login/google/callback`
 );
